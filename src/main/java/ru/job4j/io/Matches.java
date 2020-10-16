5314fd19473d252e1b6fe3b0e054ff06184b1f07
Dmitriy Yugai <studentnstu97@mail.ru>
2020-10-16 11:49:51 +0700

    2. Scanner и чтение числа из консоли_испр2

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -6,38 +6,36 @@ public class Matches {
     private int numberOfMatches = 11;
     private Scanner input = new Scanner(System.in);
 
-    private void playerEnter(int numberOfPlayer) {
-        int num = 0;
-        while (num < 1 || num > 3) {
-            System.out.println("Игрок " + numberOfPlayer + " введите число от 1 до 3:");
-            try {
-                num = Integer.valueOf(input.nextLine());
-            } catch (NumberFormatException e) {
-                num = 0;
-            }
-            if (num >= 1 && num <= 3) {
-                break;
-            } else {
-                System.out.println("Введено некорректное значение. "
-                        + "Каждый игрок за один ход может забрать от 1 до 3 спичек");
-            }
-        }
-        numberOfMatches = numberOfMatches - num;
-        if (numberOfMatches > 0) {
-            System.out.println("Игрок " + numberOfPlayer + " сделал свой ход. "
-                    + "Спичек осталось: " + numberOfMatches);
-        } else {
-            System.out.println("Игрок " + numberOfPlayer + " победил!");
-        }
-    }
-
     public static void main(String[] args) {
         Matches m = new Matches();
+        int numberOfPlayer = 1;
         System.out.println("Начало игры. Всего спичек: 11");
         while (m.numberOfMatches > 0) {
-            m.playerEnter(1);
-            m.playerEnter(2);
-            m.playerEnter(3);
+            int num = 0;
+            while (num < 1 || num > 3) {
+                System.out.println("Игрок " + numberOfPlayer + " введите число от 1 до 3:");
+                try {
+                    num = Integer.valueOf(m.input.nextLine());
+                } catch (NumberFormatException e) {
+                    num = 0;
+                }
+                if (num >= 1 && num <= 3) {
+                    break;
+                } else {
+                    System.out.println("Введено некорректное значение. "
+                            + "Каждый игрок за один ход может забрать от 1 до 3 спичек");
+                }
+            }
+            m.numberOfMatches = m.numberOfMatches - num;
+            if (m.numberOfMatches > 0) {
+                System.out.println("Игрок " + numberOfPlayer + " сделал свой ход. "
+                        + "Спичек осталось: " + m.numberOfMatches);
+                if (++numberOfPlayer > 3) {
+                    numberOfPlayer = 1;
+                }
+            } else {
+                System.out.println("Игрок " + numberOfPlayer + " победил!");
+            }
         }
     }
 }
