e49dca9f5f5ffb45b1590fc76573e9b7052d5dc4
Dmitriy Yugai <studentnstu97@mail.ru>
2020-10-17 19:30:59 +0700

    0.2. Кидаем исключение - throw new RuntimeException [#358135]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -2,11 +2,11 @@ package ru.job4j.ex;
 
 public class Fact {
 
-    @SuppressWarnings("checkstyle:LineLength")
     public int calc(int n) {
         int rsl = 1;
         if (n < 0) {
-            throw new IllegalArgumentException("Невозможно вычислить факториал отрицательного числа");
+            throw new IllegalArgumentException("Невозможно вычислить "
+                                                + "факториал отрицательного числа");
         } else {
             for (int index = 1; index <= n; index++) {
                 rsl *= index;
