0fb80f4e988506c75a727dc0f112ce61d04a3fe1
Dmitriy Yugai <studentnstu97@mail.ru>
2020-10-25 14:11:00 +0700

    6. Функции высшего порядка [#357916]

---
+++ src/main/java/ru/job4j/lambda/PhoneDictionary.java
@@ -0,0 +1,26 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        Predicate<Person> firstCond = person -> person.getName().equals(key);
+        Predicate<Person> combine = firstCond.or(person -> person.getSurname().equals(key))
+                                             .or(person -> person.getAddress().equals(key))
+                                             .or(person -> person.getPhone().equals(key));
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (combine.test(person)) {
+                result.add(person);
+            }
+        }
+        return result;
+    }
+}
