03d5523acffc7138ddb758f08f1eb6c27166d4a1
Dmitriy Yugai <studentnstu97@mail.ru>
2020-10-26 13:12:48 +0700

    0. Stream API [#357929]

---
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamUsage {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(2, -1, 5, 3, 6, -4, -8, 8, -10);
+        System.out.println(numbers);
+        List<Integer> rsl = numbers.stream().filter(e -> e > 0).collect(Collectors.toList());
+        System.out.println(rsl);
+    }
+}
