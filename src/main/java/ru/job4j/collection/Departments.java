fd40d8e31cc7f15e9b6b63ac8700c156753b4a74
Dmitriy Yugai <studentnstu97@mail.ru>
2020-10-26 18:50:09 +0700

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#357928]

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -1,20 +1,24 @@
 package ru.job4j.collection;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Departments {
 
     public static List<String> fillGaps(List<String> deps) {
-        Set<String> tmp = new LinkedHashSet<>();
-        for (String value : deps) {
-            String start = "";
-            for (String el : value.split("/")) {
-                start += el;
-                tmp.add(start);
-                start += "/";
-            }
-        }
-        return new ArrayList<>(tmp);
+        return deps.stream()
+                .flatMap(element -> {
+                    String[] elements = new String[element.split("/").length];
+                    String start = "";
+                    for (int i = 0; i < element.split("/").length; i++) {
+                        start += element.split("/")[i];
+                        elements[i] = start;
+                        start += "/";
+                    }
+                    return Arrays.stream(elements);
+                })
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     public static void sortAsc(List<String> orgs) {
