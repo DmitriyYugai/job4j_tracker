372aa84cf628cca9696b3f947a8ef1c4c56e72a9
Dmitriy Yugai <studentnstu97@mail.ru>
2020-11-10 13:47:47 +0700

    1. Sql схема для проекта Tracker. [#393942]

---
+++ db/insert.sql
@@ -0,0 +1,7 @@
+insert into items(name) values('car');
+insert into items(name) values('pen');
+insert into items(name) values('book');
+insert into items(name) values('shop');
+insert into items(name) values('computer');
+
+select* from items;
